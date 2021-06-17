a7e3f58fa95f5f02da42cac2e945be57cb35fb63
masteryodaa <kujjwal42@gmail.com>
2021-06-17 22:10:41 +0530

    link fix

--- blueprints/landingPage/views.py
+++ blueprints/landingPage/views.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect, url_for, request
+import mysql.connector
+
+conn = mysql.connector.connect(host='remotemysql.com',user='kEOWWtr4ag', password='dgXWQcifF8', database='kEOWWtr4ag')
+cursor = conn.cursor()
 
 landingPage = Blueprint('landingPage',__name__, template_folder='templates', static_folder='static',static_url_path='landingPage/static')
 
@@ -8,4 +12,37 @@ def home():
 
 @landingPage.route('/signin', methods=['POST', 'GET'])
 def signin():
-    return render_template('landingPage/signin.html')
+    # return render_template('landingPage/signin.html')
+
+    if "user" in session:
+        return redirect(url_for('.dashboard'))
+    else:
+        if request.method=='POST':
+            if request.form['email'] and request.form['password']:
+                email = request.form['email']
+                password = request.form['password']
+                
+                cursor.execute(("SELECT * FROM users WHERE email LIKE '{}'".format(email)))
+                users = cursor.fetchall()
+
+                session['user'] = users[0][1]
+
+                if len(users)>0:
+                    return redirect(url_for('.dashboard'))
+                else:
+                    return render_template('landingPage/signin.html')
+                    
+        else:
+            return render_template('landingPage/signin.html')
+
+@landingPage.route('/dashboard')
+def dashboard():
+	if "user" in session:
+		return render_template('landingPage/dashboard.html')
+	else:
+		return redirect(url_for('.signin'))
+
+@landingPage.route('/logout', methods=['POST', 'GET'])
+def logout():
+	session.pop('user')
+	return redirect(url_for('.signin'))
